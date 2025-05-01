02386c0cfecb0173a99b96f373af638114081b89
2khan <2khanvas@gmail.com>
2025-05-02 07:37:16 +0800

    populate

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-medium">Blendvision CMS</h1>
+    </div>
+  )
+}
