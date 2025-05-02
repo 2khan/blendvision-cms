e682adcd83384e0a0fe793eb6157127014d3093f
2khan <2khanvas@gmail.com>
2025-05-02 09:41:36 +0800

    routes fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Routes from './Routes'
+import Routes from './routes'
 import './index.css'
 
 const router = createBrowserRouter(Routes)
