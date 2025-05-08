d5d3f87df074ded74b47299270853d34a938b3c6
2khan <2khanvas@gmail.com>
2025-05-08 18:06:06 +0800

    update icon

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,5 +1,9 @@
 import type { TRouteObject } from '@/shared/types/utils/route'
-import { FileVideoIcon, LayoutDashboardIcon, Users2Icon } from 'lucide-react'
+import {
+  LayoutDashboardIcon,
+  Users2Icon,
+  GraduationCapIcon
+} from 'lucide-react'
 
 const ProtectedRoutes = [
   {
@@ -15,7 +19,7 @@ const ProtectedRoutes = [
   },
   {
     meta: { title: 'Course Management' },
-    icon: FileVideoIcon,
+    icon: GraduationCapIcon,
     path: '/course',
     lazy: async () => {
       const { default: Component } = await import('@/pages/course-management')
