09e129d01f8d6569ef581a78199e76d5d44e69f5
2khan <2khanvas@gmail.com>
2025-05-16 18:20:56 +0800

    add zod

--- src/pages/courses/course-edit/course-preview/index.tsx
+++ src/pages/courses/course-edit/course-preview/index.tsx
@@ -1,3 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import CourseCard from '@/components/course/course-card'
 import CoverCard from '@/components/course/cover-card'
 import PreviewCard from '@/components/custom/showcase'
@@ -11,12 +15,26 @@ interface TProps {
   course: TCourse
 }
 
+const EditCourseSchema = z.object({
+  title: z.string().min(1).optional(),
+  desc: z.string().min(1).optional(),
+  thumbnail_url: z.string().min(1).optional(),
+  tags: z.array(z.string().min(1)).optional()
+})
+
+type TParams = z.infer<typeof EditCourseSchema>
+
 export default function CoursePreview(props: TProps) {
   const { course } = props
 
+  const form = useForm<TParams>({
+    resolver: zodResolver(EditCourseSchema),
+    defaultValues: {}
+  })
+
   return (
     <TabsContent value="preview" className="flex w-full gap-3">
-      <div className="flex flex-col grow gap-3">
+      <div className="grow space-y-3">
         <PreviewCard title="Hero View">
           <CoverCard course={course} />
         </PreviewCard>
@@ -24,8 +42,6 @@ export default function CoursePreview(props: TProps) {
         <PreviewCard title="Card View">
           <CourseCard course={course} />
         </PreviewCard>
-
-        <div className="w-full h-80"></div>
       </div>
 
       <CourseEditForm />
