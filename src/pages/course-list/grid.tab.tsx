7c016012fa64a0b2bfb60e223a992c69a75816af
Jargalsaikhan Erdenetsetseg <2khanvas@gmail.com>
2025-05-29 11:33:36 +0800

    prepare

--- src/pages/course-list/grid.tab.tsx
+++ src/pages/course-list/grid.tab.tsx
@@ -1,10 +1,9 @@
+// import GridViewCard from '@/components/course/grid-view-card'
+import { PlusCircleIcon } from 'lucide-react'
+
+import { dx } from '@/lib/dx'
+
 import GridViewCard from '@/components/course/grid-view-card'
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
 import { TabsContent } from '@/components/ui/tabs'
 
 import { useCourses } from '@/shared/queries/course/course-list'
@@ -13,20 +12,30 @@ export default function GridView() {
   const { data: courses, isSuccess: coursesReady } = useCourses()
   return (
     coursesReady && (
-      <TabsContent
-        value="card"
-        className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-3 auto-rows-min"
-      >
-        {courses.map((course) => (
-          <GridViewCard key={course.id} course={course} />
-        ))}
+      <TabsContent value="card" className="flex flex-col">
+        {
+          <div className="max-w-lg mx-auto flex flex-col items-center text-center">
+            <span className={dx('fluid-heading-04', 'mb-4')}>
+              Create courses to get started
+            </span>
+            <p className={dx('fluid-paragraph-01', 'mb-4')}>
+              You haven’t created any courses yet. Click the button below to
+              start building your first course.
+            </p>
 
-        <Card className="bg-muted border border-dashed">
-          <CardHeader>
-            <CardTitle>Create Course</CardTitle>
-            <CardDescription>Create Course</CardDescription>
-          </CardHeader>
-        </Card>
+            <button>
+              <PlusCircleIcon />
+
+              <span>Create a new course</span>
+              <span>Create a new course</span>
+            </button>
+          </div>
+        }
+        <div className="grid gap-3 auto-rows-min grid-cols-[repeat(auto-fill,minmax(20rem,1fr))]">
+          {courses.map((course) => (
+            <GridViewCard key={course.id} course={course} />
+          ))}
+        </div>
       </TabsContent>
     )
   )
